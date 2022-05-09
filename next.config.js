0e875dc6eecf0f280185f300adc7b3cfd1f7e513
Phan Quoc Hung <hungdh10dt@gmail.com>
2022-05-10 00:30:25 +0700

    update next-config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+  eslint: {
+    dirs: ["pages", "src"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
