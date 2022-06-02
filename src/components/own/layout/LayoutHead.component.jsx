658d36dd1ff73d1db9ed24a5f1e2db1da81a8f6c
Phan Quoc Hung <hungdh10dt@gmail.com>
2022-06-02 17:31:31 +0700

    add header menu sp

--- src/components/own/layout/LayoutHead.component.jsx
+++ src/components/own/layout/LayoutHead.component.jsx
@@ -1,39 +1,43 @@
 /* eslint-disable @next/next/no-page-custom-font */
 /* eslint-disable @next/next/no-css-tags */
-import Head from "next/head";
+import Head from 'next/head';
 
-const TITLE = "TOAN PHAT";
+const TITLE = 'TOAN PHAT';
 
 const LayoutHead = ({ SEOInfo }) => (
   <Head>
     <title>{SEOInfo?.title || TITLE}</title>
     <meta
-      name="viewport"
-      content="minimum-scale=1, initial-scale=1, width=device-width"
+      name='viewport'
+      content='minimum-scale=1, initial-scale=1, width=device-width'
     />
-    <link rel="icon" href="/favicon.ico" />
-    <link href="/fonts/all.min.css" rel="stylesheet" />
+    <link rel='icon' href='/favicon.ico' />
+    <link href='/fonts/all.min.css' rel='stylesheet' />
+
+    <link rel='preconnect' href='https://fonts.googleapis.com' />
+    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;700&family=Tinos&display=swap"
-      rel="stylesheet"
+      href='https://fonts.googleapis.com/css2?family=Lato:wght@300;500;600;700&family=Overpass:wght@300;500;600;700&display=swap'
+      rel='stylesheet'
     />
-    <meta name="title" content={SEOInfo?.title || TITLE} />
-    <meta name="description" content={SEOInfo?.desc || ""} />
-    <meta name="image" content={SEOInfo?.image || ""} />
 
-    <meta name="og:title" content={SEOInfo?.title || TITLE} />
-    <meta name="og:description" content={SEOInfo?.desc || ""} />
-    <meta name="og:image" content={SEOInfo?.image || ""} />
+    <meta name='title' content={SEOInfo?.title || TITLE} />
+    <meta name='description' content={SEOInfo?.desc || ''} />
+    <meta name='image' content={SEOInfo?.image || ''} />
 
-    <meta itemProp="name" content={SEOInfo?.title || TITLE} />
-    <meta itemProp="description" content={SEOInfo?.desc || ""} />
-    <meta itemProp="image" content={SEOInfo?.image || ""} />
+    <meta name='og:title' content={SEOInfo?.title || TITLE} />
+    <meta name='og:description' content={SEOInfo?.desc || ''} />
+    <meta name='og:image' content={SEOInfo?.image || ''} />
 
-    <meta property="og:url" content={SEOInfo.url || ""} />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={SEOInfo?.title || TITLE} />
-    <meta property="og:description" content={SEOInfo?.desc || ""} />
-    <meta property="og:image" content={SEOInfo?.image || ""} />
+    <meta itemProp='name' content={SEOInfo?.title || TITLE} />
+    <meta itemProp='description' content={SEOInfo?.desc || ''} />
+    <meta itemProp='image' content={SEOInfo?.image || ''} />
+
+    <meta property='og:url' content={SEOInfo.url || ''} />
+    <meta property='og:type' content='website' />
+    <meta property='og:title' content={SEOInfo?.title || TITLE} />
+    <meta property='og:description' content={SEOInfo?.desc || ''} />
+    <meta property='og:image' content={SEOInfo?.image || ''} />
   </Head>
 );
 
--- src/components/own/layout/header/Header.component.jsx
+++ src/components/own/layout/header/Header.component.jsx
@@ -1,37 +1,118 @@
+import React, { useRef, useState } from 'react';
+
+import { useClickOutside } from 'components/hook';
 import styles from './Header.module.scss';
 import Image from 'next/image';
+import { produce } from 'immer';
+import clsx from 'clsx';
 
 import toan_phat_icon from '@assets/header/ToanPhat-Icon.png';
+import toan_phat_mobile_icon from '@assets/header/ToanPhat-Mobile-Icon.png';
 import language_icon from '@assets/header/language.png';
 import caret_down from '@assets/header/caret-down.png';
+import setting_icon from '@assets/header/Setting-Icon.png';
+
+const dataPopup = {
+  open: false,
+};
 
 const Header = () => {
+  const ref = useRef();
+  const [openMenu, setOpenMenu] = useState(dataPopup);
+
+  useClickOutside(ref, () =>
+    setOpenMenu(
+      produce((data) => {
+        data.open = false;
+      })
+    )
+  );
+
   return (
     <>
       <div className={styles['container']}>
-        <div className={styles['icon-toan-phat']}>
-          <Image
-            src={toan_phat_icon}
-            alt=''
-            width={56}
-            height={46}
-            layout='responsive'
-            objectFit='contain'
-            quality={100}
-          />
+        <div
+          ref={ref}
+          className={clsx(styles['wrapper'], {
+            [`${styles['wrapper']} ${styles['active']}`]: openMenu.open,
+          })}
+        >
+          <div className={styles['icon-toan-phat']}>
+            <div className={styles['icon-toan-phat__icon-desktop']}>
+              <Image
+                src={toan_phat_icon}
+                alt=''
+                width={56}
+                height={46}
+                layout='responsive'
+                objectFit='contain'
+                quality={100}
+              />
+            </div>
+            <div className={styles['icon-toan-phat__icon-mobile']}>
+              <Image
+                src={toan_phat_mobile_icon}
+                alt=''
+                width={56}
+                height={46}
+                layout='responsive'
+                objectFit='contain'
+                quality={100}
+              />
+            </div>
+          </div>
+          <div className={styles['menu']}>
+            <div className={styles['menu__item']}>Giới thiệu</div>
+            <div className={styles['menu__line']}></div>
+            <div className={styles['menu__item']}>Lĩnh vực tiên phong</div>
+            <div className={styles['menu__line']}></div>
+            <div className={styles['menu__item']}>Mục tiêu phát triển</div>
+            <div className={styles['menu__line']}></div>
+            <div className={styles['menu__item']}>Khách hàng</div>
+            <div className={styles['menu__line']}></div>
+            <div className={styles['menu__item']}>Hỗ trợ</div>
+            <div className={styles['menu__line']}></div>
+            <div className={styles['menu__item']}>Liên hệ</div>
+            <div className={styles['menu__line']}></div>
+          </div>
+          <div className={styles['language']}>
+            <div className={styles['language__icon']}>
+              <Image
+                src={language_icon}
+                alt=''
+                width={24}
+                height={24}
+                layout='responsive'
+                objectFit='contain'
+                quality={100}
+              />
+            </div>
+            <div className={styles['language__caret-down']}>
+              <Image
+                src={caret_down}
+                alt=''
+                width={16}
+                height={16}
+                layout='responsive'
+                objectFit='contain'
+                quality={100}
+              />
+            </div>
+          </div>
         </div>
-        <div className={styles['menu']}>
-          <div className={styles['menu__item']}>Giới thiệu</div>
-          <div className={styles['menu__item']}>Lĩnh vực tiên phong</div>
-          <div className={styles['menu__item']}>Mục tiêu phát triển</div>
-          <div className={styles['menu__item']}>Khách hàng</div>
-          <div className={styles['menu__item']}>Hỗ trợ</div>
-          <div className={styles['menu__item']}>Liên hệ</div>
-        </div>
-        <div className={styles['language']}>
-          <div className={styles['language__icon']}>
+        <div className={styles['menu-mobile']}>
+          <div
+            className={styles['menu-mobile__setting-icon']}
+            onClick={() =>
+              setOpenMenu(
+                produce((data) => {
+                  data.open = true;
+                })
+              )
+            }
+          >
             <Image
-              src={language_icon}
+              src={setting_icon}
               alt=''
               width={24}
               height={24}
@@ -40,18 +121,23 @@ const Header = () => {
               quality={100}
             />
           </div>
-          <div className={styles['language__caret-down']}>
+          <div className={styles['menu-mobile__toan-phat-icon']}>
             <Image
-              src={caret_down}
+              src={toan_phat_icon}
               alt=''
-              width={16}
-              height={16}
+              width={56}
+              height={46}
               layout='responsive'
               objectFit='contain'
               quality={100}
             />
           </div>
         </div>
+        <div
+          className={clsx(styles['header-backdrop'], {
+            [`${styles['header-backdrop']} ${styles['active']}`]: openMenu.open,
+          })}
+        ></div>
       </div>
     </>
   );
