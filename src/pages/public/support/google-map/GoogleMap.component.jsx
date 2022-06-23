80fbc84fc3d18c559cdc26d37d098ad652b47942
Phan Quoc Hung <hungdh10dt@gmail.com>
2022-06-24 02:52:48 +0700

    add google map

--- src/pages/public/support/google-map/GoogleMap.component.jsx
+++ src/pages/public/support/google-map/GoogleMap.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './GoogleMap.module.scss';
+import Script from 'next/script';
 import Image from 'next/image';
 import map from '@assets/support/google-map-image.png';
+
+const GOOGLE_MAPS_API_KEY = `AIzaSyCZTFFKgF3K61I8cQ3CcS4mtezeP2U08CM`;
+const source = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_MAPS_API_KEY}&libraries=places`;
 const GoogleMap = () => {
   return (
     <>
       <div className={styles['container']}>
-        <Image
+        <Script type='text/javascript' src={source} />
+        {/* <Image
           src={map}
           alt=''
           width={1440}
@@ -14,7 +19,7 @@ const GoogleMap = () => {
           layout='responsive'
           objectFit='contain'
           quality={100}
-        />
+        /> */}
       </div>
     </>
   );
