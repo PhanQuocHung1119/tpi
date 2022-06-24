60a145b908412cad09f8eae8b6090c88d6bec6b7
Phan Quoc Hung <hungdh10dt@gmail.com>
2022-06-24 15:24:57 +0700

    add anim SubMethodIrradiation page

--- src/pages/public/food-irradiation/method-irradiation/sub-method-irradiation/SubMethodIrradiation.component.jsx
+++ src/pages/public/food-irradiation/method-irradiation/sub-method-irradiation/SubMethodIrradiation.component.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import { SubMethodStep } from './sub-method-step';
+import React, { useRef } from 'react';
 import styles from './SubMethodIrradiation.module.scss';
+import { SubMethodStep } from './sub-method-step';
+import { useObserverItem } from 'components/hook/useObserverItem';
 import Image from 'next/image';
 
 import method_image from '@assets/food-irradiation/method-image.png';
 
 const SubMethodIrradiation = () => {
+  const refContainer = useRef();
+  const refContent = useRef();
+
+  useObserverItem(refContainer, styles);
+  useObserverItem(refContent, styles);
+
   return (
     <>
-      <div className={styles['container']}>
+      <div className={styles['container']} ref={refContainer}>
         <div className={styles['content-left']}>
           <div className={styles['content-left__title']}>Phương án 1:</div>
-          <div className={styles['content-left__step']}>
+          <div className={styles['content-left__step']} ref={refContent}>
             <SubMethodStep number={1} fullLine={true} />
             <SubMethodStep number={2} fullLine={true} />
             <SubMethodStep number={3} fullLine={false} />
