b86f61e9b15b3460f9ec8b38ec52a6b5541fb396
Phan Quoc Hung <hungdh10dt@gmail.com>
2023-03-22 17:48:23 +0700

    replace next link to useRoute in Submarket component

--- src/pages/public/market/sub-market/SubMarket.component.jsx
+++ src/pages/public/market/sub-market/SubMarket.component.jsx
@@ -1,15 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 import styles from './SubMarket.module.scss';
 import { get } from 'lodash';
 import Image from 'next/image';
-import Link from 'next/link';
 import { RoutePages } from '@constants/router';
 import { useObserverItem } from 'components/hook/useObserverItem';
+import { useRouter } from 'next/router';
 
 const SubMarket = ({ news }) => {
   const { title, image, desc, slug, time, author, avatar } = news.fields;
   const refContent = useRef(null);
   useObserverItem(refContent, styles);
+  const router = useRouter();
+
+  const redirectToPage = useCallback(
+    (_link) => {
+      router.push(_link);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },
+    [router]
+  );
 
   let _desc = get(desc, ['content', 0, 'content', 0, 'value']);
   let _image = get(image, ['fields', 'file', 'url']);
@@ -51,23 +60,27 @@ const SubMarket = ({ news }) => {
   return (
     <>
       <div className={styles['container']} ref={refContent}>
-        <Link href={RoutePages.MARKET + `/${slug}`} passHref>
-          <div className={styles['main-image']}>
-            <Image
-              src={`https:${_image}`}
-              alt=''
-              width={_imageWidth ? _imageWidth : 383}
-              height={_imageHeight ? _imageHeight : 200}
-              layout='responsive'
-              objectFit='contain'
-              quality={100}
-            />
-          </div>
-        </Link>
+        <div
+          className={styles['main-image']}
+          onClick={() => redirectToPage(RoutePages.MARKET + `/${slug}`)}
+        >
+          <Image
+            src={`https:${_image}`}
+            alt=''
+            width={_imageWidth ? _imageWidth : 383}
+            height={_imageHeight ? _imageHeight : 200}
+            layout='responsive'
+            objectFit='contain'
+            quality={100}
+          />
+        </div>
         <div className={styles['content-wrapper']}>
-          <Link href={RoutePages.MARKET + `/${slug}`} passHref>
-            <div className={styles['title']}>{title}</div>
-          </Link>
+          <div
+            className={styles['title']}
+            onClick={() => redirectToPage(RoutePages.MARKET + `/${slug}`)}
+          >
+            {title}
+          </div>
           <div className={styles['time']}>{convertTime(time)}</div>
           <div className={styles['desc']}>{_desc}</div>
           <div className={styles['author']}>
